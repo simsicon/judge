9365f3e75ef40c9e74ce07df2a321dde2f4a34f0
simsicon <simsicon@gmail.com>
2013-06-04 15:11:40 +0800

    Fix a typo

--- lib/judge/trainer.rb
+++ lib/judge/trainer.rb
@@ -100,7 +100,7 @@ module Judge
       @corpus_yaml_file_path ||= begin
         _dir = File.join(Judge.root, 'corpus')
         _path = File.join(_dir, 'corpus.yml')
-        Dir.mkdir(_dir) unless Dir.exist?('corpus')
+        Dir.mkdir(_dir) unless Dir.exist?(_dir)
         File.open(_path, 'w'){} unless File.exist? _path
         _path
       end
